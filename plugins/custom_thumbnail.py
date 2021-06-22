8641a67a62909e7323964f54dc74d613c2499836
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-06-22 18:03:09 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -118,7 +118,7 @@ async def save_photo(bot, update):
         )
 
 
-@pyrogram.Client.on_message(pyrogram.filters.command(["deletethumb"]))
+@pyrogram.Client.on_message(pyrogram.filters.command(["delthumb"]))
 async def delete_thumbnail(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
         await bot.delete_messages(
@@ -127,7 +127,7 @@ async def delete_thumbnail(bot, update):
             revoke=True
         )
         return
-    TRChatBase(update.from_user.id, update.text, "deletethumb")
+    TRChatBase(update.from_user.id, update.text, "delthumb")
     download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id)
     try:
         os.remove(download_location + ".jpg")
