53ea5959c3ca124edc4c4af98dab224dce5fe2b3
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-06-22 11:57:04 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -5,8 +5,7 @@ class Translation(object):
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
-    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
-URL | filename | username | password"""
+    SET_CUSTOM_USERNAME_PASSWORD = ""
     NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
     DOWNLOAD_START = "<i>DownloadIng To My Server"
     UPLOAD_START = "<i>Trying To Upload"
