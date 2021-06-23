3a54f6073b6bd2951ed9f6694823e7c5e9b6e630
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-06-23 11:07:30 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -324,7 +324,7 @@ async def youtube_dl_call_back(bot, update):
             media_album_p = []
             if images is not None:
                 i = 0
-                caption = "**Share :- @HiroshiBots**"
+                caption = "Share :- @HiroshiBots"
                 if is_w_f:
                     caption = "/upgrade to Plan D to remove the watermark\n© @AnyDLBot"
                 for image in images:
